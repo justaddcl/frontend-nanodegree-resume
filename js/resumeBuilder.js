a00dc7c7fc88b84664b69141ed66ef77d121489f
Yuji Nelson <k.yuji.nelson@gmail.com>
2016-07-01 15:00:46 -0700

    Completed the Data Types section
    Completed the Data Types section of the course and cleared resumeBulder.js with the exception of the bio, education, work, and projects objects.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,29 +1,38 @@
 /*
 	This is empty on purpose! Your code to build the resume will go here.
 */
-var name = "Yuji Nelson";
-var role = "Front-End Developer / Designer";
-// $('#main').append("Yuji Nelson");
-var thoughts = "I am Yuji Nelson and God is AWESOME!";
+
+
 var bio = {
 	"name" : "Yuji",
 	"role" : "Front-End Developer / Designer",
 	"contactInfo" : "https://www.linkedin.com/in/yujinelson",
 	"pictureURL" : "https://media.licdn.com/mpr/mpr/shrinknp_200_200/p/8/005/02b/1cf/22e6f50.jpg",
 	"welcome" : "Welcome",
-	"skills" : ["HTML", "CSS" ,"JavaScript"]
+	"skills" : ["HTML", "CSS", "JavaScript"]
 };
 
-// var moreThoughts = thoughts.replace("AWESOME", "GREAT!");
-// $('#main').append(moreThoughts);
+var education = {
+	"name" : "UW",
+	"years" : "2011 - 2014",
+	"city" : "Seattle"
+};
 
-var formattedName = HTMLheaderName.replace("%data%", bio.name);
+var work = {
+	"position" : "Front-End Developer / Designer",
+	"employer" : "Boeing",
+	"years" : "2014-Present",
+	"city" : "Bellevue"
+};
 
-var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+var projects = {
+	"projects" : [
+		{
+			"title" : "Sample Project 1",
+			"dates" : "2014",
+			"description" : "Who moved my chesse cheesy feet cauliflower cheese. Queso taleggio when the cheese comes out everybody's happy airedale ricotta cheese and wine paneer camembert de normandie. Swiss mozzarella cheese slices feta fromage frais airedale swiss cheesecake. Hard cheese blue castello halloumi parmesan say cheese stinking bishop jarlsber"
+		}
+	]
+};
 
-$('#header').prepend(bio.skills);
-$('#header').prepend(bio.welcome);
-$('#header').prepend(bio.pictureURL);
-$('#header').prepend(bio.contactInfo);
-$('#header').prepend(formattedRole);
-$('#header').prepend(formattedName);
+
